0aefffa18033adea506c36f70910e75c169a78a0
Gildon Kirk <gildonkirk@gmail.com>
2017-10-09 11:05:47 -0400

    import each component into view that represents a page, then imported those views into app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
-import ArtistJumbotron from './components/ArtistPage/ArtistJumbotron';
-import EventModal from './components/ArtistPage/EventModal';
-import GridContainer from './components/ArtistPage/GridContainer';
-import GridItem from './components/ArtistPage/GridItem';
-import LoginForm from './components/SignInPage/LoginForm';
-import Nav from './components/Homepage/Nav';
-import SignUpModal from './components/SignInPage/SignUpModal';
-import Tweets from './components/ArtistPage/Tweets';
-import Artist from './components/Homepage/Artist';
-import ProfileContainer from './components/Homepage/ProfileContainer';
+
+import SignInPage from './views/SignInPage';
+import Homepage from './views/Homepage';
+import ArtistPage from './views/ArtistPage';
+
+
+// const App = () => (
+//   <Router>
+//     <div>
+//       <ul>
+//         <li><Link to="/">Sign In</Link></li>
+//         <li><Link to="/index">Home</Link></li>
+//         <li><Link to="/artist">Artist</Link></li>
+//       </ul>
+//
+//       <hr/>
+//
+//       <Route exact path="/" component={Home}/>
+//       <Route path="/about" component={About}/>
+//       <Route path="/topics" component={Topics}/>
+//     </div>
+//   </Router>
+// )
+// export default App;
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ArtistJumbotron />
-        <EventModal />
-        <GridContainer />
-        <GridItem />
-        <LoginForm />
-        <Nav />
-        <SignUpModal />
-        <Tweets />
-        <Artist />
-        <ProfileContainer />
+        <ArtistPage />
+        <Homepage />
+        <SignInPage />
       </div>
     );
   }
