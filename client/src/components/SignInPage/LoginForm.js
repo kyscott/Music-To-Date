0b5389513d6bed1207e0cfa3e5ece8ed864546f4
Gildon Kirk <gildonkirk@gmail.com>
2017-10-09 12:31:03 -0400

    id changes in signin form, submit of modal form now closes modal

--- client/src/components/SignInPage/LoginForm.js
+++ client/src/components/SignInPage/LoginForm.js
@@ -9,14 +9,14 @@ const LoginForm = () => {
   return (
     <form>
       <div className="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+        <label for="SignInEmail">Email address</label>
+        <input type="email" className="form-control" id="SignInEmail" aria-describedby="emailHelp" placeholder="Enter email" />
       </div>
       <div className="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+        <label for="SignInPassword">Password</label>
+        <input type="password" className="form-control" id="SignInPassword" placeholder="Password" />
       </div>
-      <Link to="/home"><button type="submit" className="btn btn-primary">Submit</button></Link>
+      <Link to="/home"><button type="submit" className="btn btn-primary" data-dismiss='modal'>Submit</button></Link>
     </form>
   )
 }
