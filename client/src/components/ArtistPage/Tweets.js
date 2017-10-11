ffe69e622a258ae909898253d2c9bf7428884838
Kyle Scott <kylescott@Kyles-MacBook-Pro-2.local>
2017-10-11 14:07:14 -0400

    skeleton of artist page template

--- client/src/components/ArtistPage/Tweets.js
+++ client/src/components/ArtistPage/Tweets.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import './Tweets.css'
+
 
 const Tweets = () => {
   return (
-    <p>
-      Tweets
-    </p>
+<div className="col-md-2 sidebar">
+   <div className="well"><img src="https://1.bp.blogspot.com/-fYzEVSAoYpo/V1BbBMPg7EI/AAAAAAAAAHw/8mUCv6Jzs2MZtFXkwpdLdBFUVrr7FFh9ACLcB/s1600/twitter%2Blogo%2Btransparent%2B234.png" width="85%"/>
+   </div>
+</div>
   )
 }
 
--- client/src/components/Homepage/Nav.js
+++ client/src/components/Homepage/Nav.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import './Nav.css'
 
 const Nav = () => {
   return (
-    <p>
-      Nav
-    </p>
+<div>
+   <nav class="navbar navbar-default">
+      <div class="navbar-header">
+         <a href="#">
+            <img className="navbar-icon" alt="Home" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Circle-icons-volume.svg/1000px-Circle-icons-volume.svg.png"/>
+         </a>
+
+         <div className="form-inline">
+            <input type="text" placeholder="Search"/>
+            <input type="button" className="btn btn-default btn-sm" value="Search"/>
+         </div>
+      </div>
+   </nav>
+</div>
   )
 }
 
