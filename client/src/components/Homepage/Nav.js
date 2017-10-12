a4e80ad36616fb0db573cb70481f8d113be736fb
Gildon Kirk <gildonkirk@gmail.com>
2017-10-12 12:11:54 -0400

    on submittal of search input, makes api call based on input value

--- client/src/components/Homepage/Nav.js
+++ client/src/components/Homepage/Nav.js
@@ -1,43 +1,38 @@
 import React from 'react';
-import './Nav.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import './Nav.css';
 
 const Nav = props => {
   return (
-<<<<<<< HEAD
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Navbar</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+    <div>
+       <nav className="navbar navbar-default">
+          <div className="navbar-header">
+          // Changed this to use react-router
+             <Link to="/home">
+                <img className="navbar-icon" alt="Home" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Circle-icons-volume.svg/1000px-Circle-icons-volume.svg.png"/>
+             </Link>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-          </li>
-        </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
-=======
-<div>
-   <nav class="navbar navbar-default">
-      <div class="navbar-header">
-         <a href="#">
-            <img className="navbar-icon" alt="Home" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Circle-icons-volume.svg/1000px-Circle-icons-volume.svg.png"/>
-         </a>
-
-         <div className="form-inline">
-            <input type="text" placeholder="Search"/>
-            <input type="button" className="btn btn-default btn-sm" value="Search"/>
-         </div>
-      </div>
-   </nav>
-</div>
->>>>>>> 3d7f9a81f016a7c7c317fdfbc194374f43834970
+             <div className="form-inline">
+                <input
+                  onChange={props.handleInputChange}
+                  value={props.value}
+                  type="text"
+                  placeholder="Search"
+                />
+                <input
+                  onClick={props.handleFormSubmit}
+                  type="button"
+                  className="btn btn-default btn-sm"
+                  value="Search"
+                />
+             </div>
+          </div>
+       </nav>
+    </div>
   )
 }
 
--- client/src/components/Homepage/ProfileContainer.js
+++ client/src/components/Homepage/ProfileContainer.js
@@ -1,36 +1,8 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-const BASEURL = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=';
-const APIKEYURL = '&api_key=d66187414773dbf6291ba5b784512236&format=json';
+import React from 'react';
 
-class ProfileContainer extends Component {
-  state = {
-    result: {},
-    search: ""
-  };
-
-  componentDidMount() {
-    this.searchArtists("Kanye+West");
-  }
-
-  searchArtists = query => {
-    const queryURL = BASEURL + query + APIKEYURL;
-    console.log(queryURL);
-    axios.get(queryURL)
-      .then(
-        res => this.setState({ result: res.data.artist })
-      )
-      .catch(err => console.log(err));
-  };
-
-
-  render() {
-    return (
+const ProfileContainer = props =>
       <p>
-        {this.state.result.name}
-      </p>
-    )
-  }
-}
+        {props.artistName}
+      </p>;
 
 export default ProfileContainer;
