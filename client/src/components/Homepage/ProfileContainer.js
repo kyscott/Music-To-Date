73e34b981999b03065674502a76f3bf6d9a7afb8
Gildon Kirk <gildonkirk@gmail.com>
2017-10-12 11:04:26 -0400

    initial set up for api calls

--- client/src/components/Homepage/ProfileContainer.js
+++ client/src/components/Homepage/ProfileContainer.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+const BASEURL = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=';
+const APIKEYURL = '&api_key=d66187414773dbf6291ba5b784512236&format=json';
 
-const ProfileContainer = () => {
-  return (
-    <p>
-      Profile Container
-    </p>
-  )
+class ProfileContainer extends Component {
+  state = {
+    result: {},
+    search: ""
+  };
+
+  componentDidMount() {
+    this.searchArtists("Kanye+West");
+  }
+
+  searchArtists = query => {
+    const queryURL = BASEURL + query + APIKEYURL;
+    console.log(queryURL);
+    axios.get(queryURL)
+      .then(
+        res => this.setState({ result: res.data.artist })
+      )
+      .catch(err => console.log(err));
+  };
+
+
+  render() {
+    return (
+      <p>
+        {this.state.result.name}
+      </p>
+    )
+  }
 }
 
 export default ProfileContainer;
