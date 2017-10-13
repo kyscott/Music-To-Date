04f5ce237f3b755d330cd977ac89c4a437072879
Kyle Scott <kylescott@Kyles-MacBook-Pro-2.local>
2017-10-13 19:11:31 -0400

    fixed link on similar artist images to redirect to an api call for that specific artist and re-display the specific artist page with their info

--- client/src/components/ArtistPage/SongItem.js
+++ client/src/components/ArtistPage/SongItem.js
@@ -11,31 +11,31 @@ const SongItem = () => {
 <div className="col-md-9 top-songs-container">
 	<div className="card grid-content-container">
 	   <h4>Song Name</h4>
-	   <img className='similar-artist-img' src="https://gitlab.maikel.pro/uploads/maikeldus/WhatsSpy-Public/718515848e/team-person-placeholder.jpg" alt="artist-2"/>
+	   <img className='top-song-img' src="https://gitlab.maikel.pro/uploads/maikeldus/WhatsSpy-Public/718515848e/team-person-placeholder.jpg" alt="artist-2"/>
 	   <a className="stream-link" href="#"><i className="fa fa-music"></i> Listen Here</a>
 	</div>
 
 	<div className="card grid-content-container">
    <h4>Song Name</h4>
-   <img className='similar-artist-img' src="https://gitlab.maikel.pro/uploads/maikeldus/WhatsSpy-Public/718515848e/team-person-placeholder.jpg" alt="artist-2"/>
+   <img className='top-song-img' src="https://gitlab.maikel.pro/uploads/maikeldus/WhatsSpy-Public/718515848e/team-person-placeholder.jpg" alt="artist-2"/>
    <a className="stream-link" href="#"><i className="fa fa-music"></i> Listen Here</a>
 </div>
 
 	<div className="card grid-content-container">
 	   <h4>Song Name</h4>
-	   <img className='similar-artist-img' src="https://gitlab.maikel.pro/uploads/maikeldus/WhatsSpy-Public/718515848e/team-person-placeholder.jpg" alt="artist-2"/>
+	   <img className='top-song-img' src="https://gitlab.maikel.pro/uploads/maikeldus/WhatsSpy-Public/718515848e/team-person-placeholder.jpg" alt="artist-2"/>
 	   <a className="stream-link" href="#"><i className="fa fa-music"></i> Listen Here</a>
 	</div>
 
 	<div className="card grid-content-container">
 	   <h4>Song Name</h4>
-	   <img className='similar-artist-img' src="https://gitlab.maikel.pro/uploads/maikeldus/WhatsSpy-Public/718515848e/team-person-placeholder.jpg" alt="artist-2"/>
+	   <img className='top-song-img' src="https://gitlab.maikel.pro/uploads/maikeldus/WhatsSpy-Public/718515848e/team-person-placeholder.jpg" alt="artist-2"/>
 	   <a className="stream-link" href="#"><i className="fa fa-music"></i> Listen Here</a>
 	</div>
 
 	<div className="card grid-content-container">
 	   <h4>Song Name</h4>
-	   <img className='similar-artist-img' src="https://gitlab.maikel.pro/uploads/maikeldus/WhatsSpy-Public/718515848e/team-person-placeholder.jpg" alt="artist-2"/>
+	   <img className='top-song-img' src="https://gitlab.maikel.pro/uploads/maikeldus/WhatsSpy-Public/718515848e/team-person-placeholder.jpg" alt="artist-2"/>
 	   <a className="stream-link" href="#"><i className="fa fa-music"></i> Listen Here</a>
 	</div>
 </div>
