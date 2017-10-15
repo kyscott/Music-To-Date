f4c261fbdc5b441325ddea2fc234d4736b7606fd
Kyle Scott <kylescott@Kyles-MacBook-Pro-2.local>
2017-10-15 16:24:31 -0400

    fixed formatting problem

--- client/src/components/ArtistPage/SimilarArtists.js
+++ client/src/components/ArtistPage/SimilarArtists.js
@@ -6,40 +6,41 @@ import {
 } from 'react-router-dom';
 
 import './css/SimilarArtists.css';
+import '../../hover.css'
 
 const SimilarArtists = props => {
   return (
 <div className="col-md-9 similar-artists-container">
 
-   <div className="card grid-content-container">
+   <div className="card grid-content-container hvr-wobble-vertical">
    <a href={props.similarArtist1}>
       <h4>{props.similarArtist1}</h4>
       <img className='similar-artist-img' src={props.similarArtistImage1} alt={props.similarArtist1}/>
       </a>
    </div>
 
-   <div className="card grid-content-container">
+   <div className="card grid-content-container hvr-wobble-vertical">
    <a href={props.similarArtist2}>
       <h4>{props.similarArtist2}</h4>
       <img className='similar-artist-img' src={props.similarArtistImage2} alt={props.similarArtist2}/>
       </a>
    </div>
 
-   <div className="card grid-content-container">
+   <div className="card grid-content-container hvr-wobble-vertical">
    <a href={props.similarArtist3}>
       <h4>{props.similarArtist3}</h4>
       <img className='similar-artist-img' src={props.similarArtistImage3} alt={props.similarArtist3}/>
       </a>
    </div>
 
-   <div className="card grid-content-container">
+   <div className="card grid-content-container hvr-wobble-vertical">
    <a href={props.similarArtist4}>
       <h4>{props.similarArtist4}</h4>
       <img className='similar-artist-img' src={props.similarArtistImage4} alt={props.similarArtist4}/>
       </a>
    </div>
 
-   <div className="card grid-content-container">
+   <div className="card grid-content-container hvr-wobble-vertical">
    <a href={props.similarArtist5}>
       <h4>{props.similarArtist5}</h4>
       <img className='similar-artist-img' src={props.similarArtistImage5} alt={props.similarArtist5}/>
