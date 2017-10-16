a27dc8f6f8a7506ed344c91aa6b910ef64fce751
Sasha Slacum <slacus08@gmail.com>
2017-10-16 14:45:11 -0400

    added initial files and code into app folder for incorporating passport and express for login funciton on login page

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+//SASHA ADDED
+const passport = require('passport');
+const env = require('dotenv').load();
+const session = require('express-session');
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -16,4 +21,45 @@ app.get("*", function(req, res) {
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
+
+
+
+//SASHA ADDED ALL BELOW
+//For BodyParser
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(express.static('./app/views'));
+
+// For Passport
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+})); // session secret
+app.use(passport.initialize());
+
+//Models
+const models = require("./app/models"),
+
+//Routes
+authRoute = require('./app/routes/auth.js')(app, passport);
+
+//load passport strategies
+require('./app/config/passport/passport.js')(passport, models.user);
+
+//Sync Database
+models.sequelize.sync().then(function() {
+  console.log('Nice! Database looks fine')
+}).catch(function(err) {
+  console.log(err, "Something went wrong with the Database Update!")
+});
+
+app.listen(port, function(err) {
+  if (!err)
+    console.log("Site is live");
+  else console.log(err)
 });
